a110d7847f727d81df1adc06874d679752c0da46
Sam-Khan123 <abdussamadkhan027@gmail.com>
2025-03-26 02:54:25 +0500

    adding starting project

---
+++ BackgroundGen.js
@@ -0,0 +1,28 @@
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+
+
+
+function setGradient(){
+    body.style.background = "linear-gradient(to left, "+color1.value+", "+color2.value+")";
+    css.textContent = body.style.background + ";";
+}
+
+var color3  = document.querySelector(".color3");
+
+
+function setLoad(){
+    var random  = body.style.background = "#"+ Math.floor(Math.random()* 0xFFFFFF).toString(16)
+    color3.value = random;
+
+}
+
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+window.addEventListener("load", setLoad);
